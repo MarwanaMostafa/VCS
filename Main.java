e129c94894909166ef5e57ea22ff7310b4426cfd
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-11-03 18:01:35 +0200

    print user data 2

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Collaborator 2");
+        printWhichUser("Collaborator 2",25,"123123","1st gize");
+    }
+
+    public static void printUserDataForUser2(String userName,int age,String mobileNumber,String address){
+
+        System.out.println("User is :"+userName);
+        System.out.println("Age is :"+age);
+        System.out.println("MobileNumber is :"+mobileNumber);
+        System.out.println("Address is :"+address);
 
     }
 }
+
